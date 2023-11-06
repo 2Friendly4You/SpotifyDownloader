2731ee549d2d0c1e4fd5d1d9ad5e6bfc3dbe0e13
Tobias Rick <turboschnecke2016@gmail.com>
2023-11-06 11:54:17 +0100

    Pending requests are now saved in txt files, to fix gunicorn

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ import time
 
 app = Flask(__name__)
 
-# A list to store pending request UUIDs
-pending_requests = []
+# A file to store pending request UUIDs
+pending_requests_file = 'pending_requests.txt'
 
 def run_spotdl(unique_id, search_query, audio_format, lyrics_format, output_format):
-    global pending_requests
+    # Mark the request as pending in the shared file
+    with open(pending_requests_file, 'a') as pending_file:
+        pending_file.write(unique_id + '\n')
 
     download_folder = os.path.join('templates', 'download', unique_id)
     os.makedirs(download_folder, exist_ok=True)
@@ -36,42 +38,17 @@ def run_spotdl(unique_id, search_query, audio_format, lyrics_format, output_form
     # Remove the original folder
     shutil.rmtree(download_folder)
 
-    # Remove the unique_id from the pending_requests list after the subprocess command has finished executing
-    pending_requests.remove(unique_id)
-
-def is_request_pending(unique_id):
-    global pending_requests
-    return unique_id in pending_requests
-
-def run_spotdl(unique_id, search_query, audio_format, lyrics_format, output_format):
-    global pending_requests
-
-    download_folder = os.path.join('templates', 'download', unique_id)
-    os.makedirs(download_folder, exist_ok=True)
-
-    # Run spotdl with the provided parameters and unique folder
-    if not lyrics_format:
-        command = f'spotdl "{search_query}" --audio {audio_format} --format {output_format} --output "{download_folder}"'
-    else:
-        command = f'spotdl "{search_query}" --audio {audio_format} --lyrics {lyrics_format} --format {output_format} --output "{download_folder}"'
-
-    result = subprocess.run(command, shell=True, check=True, text=True)
-
-    if result.returncode == 0:
-        # Create a zip file with the contents of the folder
-        try:
-            shutil.make_archive(download_folder, 'zip', download_folder)
-        except FileNotFoundError as e:
-            time.sleep(1)
-            shutil.make_archive(download_folder, 'zip', download_folder)
-
-    # Remove the original folder
-    shutil.rmtree(download_folder)
-    pending_requests.remove(unique_id)
+    # Remove the request from the shared file when completed
+    with open(pending_requests_file, 'r') as pending_file:
+        lines = pending_file.readlines()
+    with open(pending_requests_file, 'w') as pending_file:
+        for line in lines:
+            if line.strip() != unique_id:
+                pending_file.write(line)
 
 @app.route('/')
 def index():
-    return render_template('index.html', pending_requests=pending_requests)
+    return render_template('index.html', pending_requests=get_pending_requests())
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -80,19 +57,12 @@ def search():
     lyrics_format = request.form['lyrics_format']
     output_format = request.form['output_format']
 
-    ## Check if the search query is empty
+    # Check if the search query is empty
     if not search_query:
         return jsonify({'status': 'error', 'message': 'Search query is required'})
 
     # Generate a new unique ID
-    while True:
-        unique_id = str(uuid.uuid4())
-        download_folder = os.path.join('templates', 'download', unique_id)
-        if not os.path.exists(download_folder):
-            break
-
-    # Add the UUID to the list of pending requests
-    pending_requests.append(unique_id)
+    unique_id = str(uuid.uuid4())
 
     # Run spotdl in a background thread
     thread = threading.Thread(target=run_spotdl, args=(unique_id, search_query, audio_format, lyrics_format, output_format))
@@ -102,7 +72,9 @@ def search():
 
 @app.route('/status/<unique_id>', methods=['GET'])
 def check_request(unique_id):
-    if is_request_pending(unique_id):
+    pending_requests = get_pending_requests()
+
+    if unique_id in pending_requests:
         return jsonify({'status': 'pending'})
     else:
         return jsonify({'status': 'completed'})
@@ -114,7 +86,14 @@ def download(unique_id):
         return send_file(download_file, as_attachment=True)
     else:
         return jsonify({'status': 'error', 'message': 'File not found'})
-        
+
+def get_pending_requests():
+    try:
+        with open(pending_requests_file, 'r') as pending_file:
+            return [line.strip() for line in pending_file.readlines()]
+    except FileNotFoundError:
+        return []
+
 if __name__ == '__main__':
+    os.makedirs('templates/download', exist_ok=True)
     app.run(host='0.0.0.0', port=5000, debug=False)
- 
